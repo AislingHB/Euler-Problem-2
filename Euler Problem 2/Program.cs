ec3e519173c8fd3ee0e059cb6b711a25b79f2f4f
AislingHB <aharrisonbond@gmail.com>
2016-06-20 16:18:10 +0100

    Add files via upload

---
+++ Euler Problem 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler_Problem_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = 0;
+            int second = 1;
+            int current = 0;
+            int total = 0;
+
+            do
+            {
+                if(current%2 == 0)
+                    total += current;
+
+                first = second;
+                second = current;
+                current = first + second;
+            }
+            while (current < 4000000);
+
+            Console.WriteLine(total);
+            Console.ReadLine();
+        }
+    }
+}
